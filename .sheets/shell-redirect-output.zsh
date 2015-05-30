ddf4fa60f3e55263dfce9a705b79ad1a52d88317
Srijan R Shetty <srijan.shetty@gmail.com>
2015-05-30 11:29:41 +0530

    Redirect shell output to a file

---
+++ .sheets/shell-redirect-output.zsh
@@ -0,0 +1,10 @@
+# Close STDOUT file descriptor
+exec 1<&-
+# Close STDERR FD
+exec 2<&-
+
+# Open STDOUT as $LOG_FILE file for read and write.
+exec 1<>$LOG_FILE
+
+# Redirect STDERR to STDOUT
+exec 2>&1
